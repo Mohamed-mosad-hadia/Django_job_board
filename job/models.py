4ed25504bbe9b4000ba0e7d7df201a27f71e5cb1
Mohamed Mosaad <muhamed.mosadd@gmail.com>
2025-02-19 04:16:57 +0200

    add job models

--- job/models.py
+++ job/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
+
+JOB_TYPE = (
+    ("Full Time", "Full Time"),
+    ("Part Time", "Part Time"),
+)
+
 
 # Create your models here.
+class Job(models.Model):  # table name will be job_job
+
+    title = models.CharField(max_length=100)  # column varchar(100)
+
+    job_type = models.CharField(max_length=15, choices=JOB_TYPE)  # column varchar(15)
+
+    description = models.TextField()  # column text
+
+    published_at = models.DateTimeField(auto_now=True)  # column datetime
+
+    vacancy = models.IntegerField(default=1)  # column integer
+
+    salary = models.IntegerField(default=0)  # column integer
+
+    experience = models.IntegerField(default=1)  # column integer
+
+    # The __str__() method returns a human-readable, or informal, string representation of an object
+    def __str__(self):
+        """this is used to show the title of the job in the admin panel instead of job object 1,2,3,4,5"""
+        return self.title
---
+++ src/outline.md
@@ -0,0 +1,42 @@
+Job :
+---------Data---------
+-title
+-location
+-job type
+-description  
+ -published at
+-vacancy
+-salary
+-category
+-experience
+
+--------Function--------
+-apply job
+-post job
+
+Blog :
+---------Data---------
+-title
+-description
+-created at
+-Category
+-tags
+-author
+
+--------Function--------
+-search
+-comment
+-recent job
+
+Contact :
+
+Home :
+
+Login:
+
+     Workflow Backend
+
+        URL: Path
+        View: Logic
+        Models: Database
+        templete : frontend
